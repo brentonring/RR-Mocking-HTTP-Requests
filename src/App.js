cf211f588929ee862bd4050657ee1bcd06dd142a
Brenton Ring <brentonring@Brentons-Air.sc.rr.com>
2022-06-28 13:35:11 -0400

    tests for correct name and URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/brentonring')
+    .then(res => res.json())
+    .then(data => {
+      setGitHubName(data.name)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href='https://github.com/brentonring'><button>Link to GitHub Profile</button></a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,21 @@
+import { render, waitFor, screen, wait } from "@testing-library/react";
+import App from './App'
 
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
 
+test('receives GitHub name from GitHub REST API using jest fetch mock', async() => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Brenton Ring'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', {level: 2}))
+    expect(gitHubName).toHaveTextContent('Brenton Ring')
+});
 
+test('receives GitHub URL from GitHub REST API using jest fetch mock', async() => {
+    fetch.mockResponseOnce(JSON.stringify({url: 'https://github.com/brentonring'}))
+    render(<App />)
+    const gitHubURL = await waitFor(() => screen.getByRole('link'))
+    expect(gitHubURL).toHaveAttribute('href', expect.stringContaining('github.com'))
+});
